dde0c612aad9edf6a8e6e99c00c4391b6c5ded7f
sam1246 <sam@apps-craft.com>
2023-12-27 15:45:02 +0000

    User experience improved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "the poem will go here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "6a33dd4o00tf5d96902aafb8b01808f5";
+  let context =
+    "You are a funny dog poem expert and you love to write short funny poems about dogs, the funny joke will include a dogs name from the instructions and be a maximum of four sentences and in basic HTML ";
+  let prompt = `User instructions: generate a funny poem about a dog called ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a funny poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
